db4897326f2e9e3921c8a67464fc8b94acf0b4d9
mmm2sp <mmm2sp@gmail.com>
2022-02-01 14:32:11 +0300

    Added first task

--- Task1.cpp
+++ Task1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "Muravev Matvei Mikhailovich" << ',' << ' ' << "Group: B02-112" << ' ' << "(LPR)" << endl;
     return 0;
 }
